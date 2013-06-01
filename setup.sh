789222cedfafabf8d820c9354a12584c2a1af5ae
orangain <orangain@gmail.com>
2013-06-01 22:15:02 +0900

    Replaced source with dot `.` command.
    For sh compatibility.

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ cwd=$( cd `dirname ${0}` && pwd )
 $cwd/mksymlinks.sh
 
 if [ -f ~/.bashrc ]; then
-    echo "source dotfiles/_bashrc" >> ~/.bashrc
+    echo ". dotfiles/_bashrc" >> ~/.bashrc
 else
-    echo "source dotfiles/_bashrc" >> ~/.bash_profile
+    echo ". dotfiles/_bashrc" >> ~/.bash_profile
 fi
 
 $(cd dotfiles; git submodule update --init)
