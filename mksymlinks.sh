c5cafba72178191dfed6a852ea057a76066b2196
orangain <orangain@gmail.com>
2017-11-03 21:03:37 +0900

    Make symlink of VSCode

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -10,3 +10,14 @@ ln -sf $cwd/_gvimrc ~/.gvimrc
 ln -sf $cwd/_gitconfig ~/.gitconfig
 ln -sf $cwd/_gitignore ~/.gitignore
 ln -sf $cwd/_jscsrc ~/.jscsrc
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	CODE_DIR=~/Library/Application\ Support/Code/User
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+	CODE_DIR=~/.config/Code/User
+fi
+
+if [[ -n "$OSTYPE" ]]; then
+	mkdir -p $CODE_DIR
+	ln -sf $cwd/Code/settings.json "$CODE_DIR/settings.json"
+fi
